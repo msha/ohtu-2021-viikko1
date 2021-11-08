9f50afd25e16d55df37036acbb4a53bf6f1aeaf3
Lauri <1960498+msha@users.noreply.github.com>
2021-11-08 19:56:20 +0200

    add unittests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,39 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_laita_liian_vahan(self):
+        maara = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, maara)
+
+    def test_laita_liikaa2(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_liikaa(self):
+        self.varasto.ota_varastosta(100)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_ota_liian_vahan(self):
+        maara = self.varasto.ota_varastosta(-5)
+        self.assertAlmostEqual(maara, 0)
+
+    def test_nega_constructor1(self):
+        self.varasto = Varasto(-5,-5)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_nega_constructor2(self):
+        self.varasto = Varasto(-5,-5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_nega_constructor2(self):
+        self.varasto = Varasto(5,10)
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+    def test_constructor(self):
+        self.varasto = Varasto(10,5)
+        s = f"varasto: {self.varasto}"
+        self.assertEqual(s.lower(), "varasto: saldo = 5, vielä tilaa 5")
+
+
